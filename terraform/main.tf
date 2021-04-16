81ae9db1026facd3fe2e7ecbacbf9bcfb9f690f8
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-16 18:27:18 +0100

    Skeleton Baseline

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    verison = " -> 2.0"
+    region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "cv-site" {
+    bucket = "cv.anhtran.co.uk"
+    acl    = "public-read"
+
+    website {
+      index_document = "../website/index.html"
+
+    }
+
+    versioning = {
+      enabled = true
+    }
+}
