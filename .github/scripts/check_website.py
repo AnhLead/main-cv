48b7ff8f809cd57d3b8c42a776ba71bd73179c38
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-19 11:25:30 +0100

    testing

--- .github/scripts/check_website.py
+++ .github/scripts/check_website.py
@@ -6,11 +6,5 @@ staged_files = repo.index.diff("HEAD")
 print("=============== list modified files ===============")
 for x in staged_files:
     print(x.a_path)
-    if website not in x.a_pth:
-        run_job = False
-        print('False')
-        break
-    else:
-        run_job = True
-        print('True')
+
 
