2257c43b65e64b20392395efb22b344bc25f44e3
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-20 00:16:06 +0100

    updated files workflow

--- .github/scripts/check_website.py
+++ .github/scripts/check_website.py
@@ -1,18 +1,12 @@
 import os
 from git import Repo
-from github import Github
 
 repo = Repo('../../../main-cv')
-staged_files = repo.index.diff("HEAD")
+staged_files = repo.index.diff('HEAD')
 print("=============== list modified files ===============")
 for x in staged_files:
     print(x)
 
 
-for item in repo.index.diff(None):
+for item in repo.index.diff('HEAD'):
     print (item)
-
-repo = g.get_repo('../../../main-cv')
-contents = repo.get_contents("")
-for content_file in contents:
-    print(content_file)
