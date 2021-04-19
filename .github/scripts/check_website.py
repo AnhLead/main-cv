afde541865f5a7bb62d82f9996c02137e5f6a23e
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-19 11:09:24 +0100

    updated check_website.py

--- .github/scripts/check_website.py
+++ .github/scripts/check_website.py
@@ -1,14 +1,16 @@
 import os
 from git import Repo
 
-repo = Repo()
+repo = Repo(self.rorepo.working_tree_dir)
 staged_files = repo.index.diff("HEAD")
 print("=============== list modified files ===============")
 for x in staged_files:
     print(x.a_path)
     if website not in x.a_pth:
         run_job = False
+        print("False")
         break
     else:
         run_job = True
+        print("True")
 
