aad1f1bcd4509c102e2881e17b89a9f50b8625eb
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-19 11:16:08 +0100

    updated check_website.py

--- .github/scripts/check_website.py
+++ .github/scripts/check_website.py
@@ -1,7 +1,7 @@
 import os
 from git import Repo
 
-repo = Repo('main-cv')
+repo = Repo('../../main-cv/')
 staged_files = repo.index.diff("HEAD")
 print("=============== list modified files ===============")
 for x in staged_files:
