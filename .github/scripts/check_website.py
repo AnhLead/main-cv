65f47a6c28ea7196caa78831606bbdd17087e0dd
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-20 00:30:43 +0100

    updated files workflow

--- .github/scripts/check_website.py
+++ .github/scripts/check_website.py
@@ -6,6 +6,9 @@ index = repo.index
 print("=============== list modified files ===============")
 
 count_modified_files = len(repo.index.diff(None))
-count_staged_files = len(repo.index.diff("HEAD"))
-print count_modified_files, count_staged_files
+count_staged_files = len(repo.index.diff('HEAD'))
+print(count_modified_files, count_staged_files)
 
+staged_files = repo.index.diff('HEAD')
+for x in staged_files:
+    print(x.a_path) 
