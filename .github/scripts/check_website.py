2ad4fd5e4d61f7d668ab25e52e9178606e20545a
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-20 00:24:11 +0100

    updated files workflow

--- .github/scripts/check_website.py
+++ .github/scripts/check_website.py
@@ -7,6 +7,11 @@ print("=============== list modified files ===============")
 for x in index.diff('HEAD'):
     print(x)
 
+for x in index.diff():
+    print(x)
+
+for x in index.diff(none):
+    print(x)
 
 hcommit = repo.head.commit
 for diff_added in hcommit.diff('HEAD~1').iter_change_type('A'):
@@ -14,9 +19,16 @@ for diff_added in hcommit.diff('HEAD~1').iter_change_type('A'):
 
 for c in repo.index.diff(None):
     print(c)
-    
+
 for b in repo.index.diff(None):
     print(b)
 
 for a in repo.untracked_files:
     print(a)
+
+for x in hcommit.diff()                  # diff tree against index
+    print(x)
+for x in hcommit.diff('HEAD~1')          # diff tree against previous tree
+    print(x)
+for x in hcommit.diff(None) 
+    print(x)
