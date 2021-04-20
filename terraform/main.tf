f3975571c186109ed03a885c97c0273d9af92c18
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-20 20:23:08 +0100

    acm cert added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,8 @@
-terraform {
-  required_version = ">= 0.12.24"
-}
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
-resource "aws_s3_bucket" "cv-site" {
-  bucket = "var.bucket_name"
-  acl    = "private"
-  tags   = local.tags
+# Main Bucket
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = "www.${var.bucket_name}"
+  acl    = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
 
   server_side_encryption_configuration {
     rule {
@@ -22,124 +15,50 @@ resource "aws_s3_bucket" "cv-site" {
   website {
     index_document = "index.html"
   }
+
+  tags = var.common_tags
 }
 
-resource "aws_s3_bucket_public_access_block" "s3block" {
-  bucket                  = name.wesite.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
 
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "origin-access-identity/${var.bucket_name}"
+# S3 bucket for redirecting non-www to www.
+resource "aws_s3_bucket" "root_site_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
 
-}
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = name.s3bucket.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
-
-
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-
-    }
-
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = var.comment
-  default_root_object = "index.html"
-
-  logging_config {
-    include_cookies = false
-    bucket          = "${var.bucket-name}.s3.amazonaws.com"
-    prefix          = var.logs_prefix
-
-  }
-
-  aliases = [
-  var.domain_aliases]
-
-  default_cache_behavior {
-    allowed_methods = [
-      "DELETE",
-      "GET",
-      "HEAD",
-      "OPTIONS",
-      "PATCH",
-      "POST",
-    "PUT"]
-    cached_methods = [
-      "GET",
-    "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
     }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
 
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern = "/*"
-    allowed_methods = [
-      "GET",
-      "HEAD",
-    "OPTIONS"]
-    cached_methods = [
-      "GET",
-      "HEAD",
-    "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers = [
-      "Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
+  website {
+    redirect_all_requests_to = "https://www.${var.domain_name}"
   }
 
-  price_class = var.price_class
+  tags = var.common_tags
+}
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
 
-    }
+# SSL Certificate
+resource "aws_acm_certificate" "ssl_certificate" {
+  provider                  = aws.acm_provider
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "EMAIL"
+  #validation_method         = "DNS"
+
+  tags = var.common_tags
+
+  lifecycle {
+    create_before_destroy = true
   }
+}
 
-  tags = {
-    Environment = var.Environment
-  }
-
-  viewer_certificate {
-    ssl_support_method             = "sni-only"
-    cloudfront_default_certificate = false
-    acm_certificate_arn            = var.acm_certificate_arn
-  }
+resource "aws_acm_certificate_validation" "cert_validation" {
+  provider        = aws.acm_provider
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
 }
---
+++ terraform/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.12.24"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "anhtran.co.uk-terraform"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
