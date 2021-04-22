72c974ffc25d1c2fa05cc69f54dd784b03253b31
AnhLead <49004270+AnhLead@users.noreply.github.com>
2021-04-22 23:47:46 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 Website Link: http://www.anhtran.co.uk
 
+
 COMPLETED: 
+
     - Website
+    
     - CloudFront
+    
     - S3 Bucket
+    
     - Route53
+    
     - HTTPS
+    
     - Partial CI/CD for frontend
-    -
+    
+    
+    
 TODO: 
+    
     - Javascript visitor Counter
+    
     - DynamoDB backend API for counter
+    
     - Lambda for API calls
+    
     - Test and Further CI/CD
--- backend/dynamodb/terraform/main.tf
+++ backend/dynamodb/terraform/main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
-  region     = "eu-west-2"
-  version    = "~> 2.0"
+  region  = "eu-west-2"
 }
 
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "anh.tran.co.uk-db-Terraform"
+  name           = var.table_name
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
@@ -22,7 +21,7 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   }
 
   tags = {
-    Name = var.common_tags
+    Name        = var.common_tags
     Environment = "production"
   }
 
