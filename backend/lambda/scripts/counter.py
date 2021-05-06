f581ee1cde3b15963b7c291782d1ab0e4ddea343
anh <anh>
2021-05-06 16:21:12 +0100

    Created Lambda Function and Counter.py

--- backend/lambda/scripts/counter.py
+++ backend/lambda/scripts/counter.py
@@ -1,13 +1,33 @@
 import os
-
 import boto3
+import decimal
+import json
 
-from flask import Flask
-app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb')
+c = dynamodb.Table('website-counter-Terraform')
 
-client = boto3.client('dynamodb')
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
+# Variables
+website = 'anhtran.co.uk'
+counter = 0  
 
-@app.route("/hello")
-def hello():
-    return "hi world"
+update = c.update_item(
+    Key={
+        'Website': website,
+        'Counter': counter
+    },
+    UpdateExpression = 'SET UpdatedCounter = UpdatedCounter + :val',
+    ExpressionAttributeValues={
+        ':val' : decimal.Decimal(1) 
+    },
+    ReturnValues = "UPDATED_NEW"
+)
--- backend/lambda/terraform/main.tf
+++ backend/lambda/terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.15.1"
+  required_version = ">= 0.12.24"
 }
 
 # AWS Region
@@ -8,48 +8,48 @@ provider "aws" {
 }
 
 # Zip the function to be run at function app
-data "archive_file" "init" { 
-    type = "zip"
-    source_file = "${path.module}/../scripts/counter.py"
-    output_patch = "${path.module}/counter.zip"
+data "archive_file" "init" {
+  type        = "zip"
+  source_file = "${path.module}/../scripts/counter.py"
+  output_path = "${path.module}/counter.zip"
 }
 
 # Create S3 Bucket
-resource = "aws_s3_bucket" "counter_bucket" {
-    bucket = "counter_s3_bucket"
-    acl = "private"
-    tags = {
-        Name = "counter_s3_bucket"
-    }
+resource "aws_s3_bucket" "counter_bucket" {
+  bucket = "adt-counter-s3-bucket"
+  acl    = "private"
+  tags = {
+    Name = "adt-counter-s3-bucket"
+  }
 }
 
 #Upload zip to s3 bucket
-resource = "aws_s3_bucket.counter_object" "object" { 
-    bucket = aws_s3_bucket.counter_s3_bucket.id
-    key = "counter.zip"
-    source = "${path.module}/counter.zip"
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.counter_bucket.id
+  key    = "counter.zip"
+  source = "${path.module}/counter.zip"
 }
 
 # IAM Role for lambda
-resource = "aws_iam_role" "counter_lambda_role" {
-    name = "counter_lambda_role"
-    assume_role_policy = file("../scripts/lambda_assume_role.json")
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = file("../scripts/lambda_assume_role.json")
 }
 
 # IAM Role-Policy for lambda
-resource = "aws_iam_role_policy" "counter_lambda_policy" {
-    name = "counter_lambda_policy"
-    role = "aws_iam.role.lambda_role.id"
-    policy = file("../scripts/counter_lambda_policy.json")
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = file("../scripts/lambda_policy.json")
 }
 
 
 # AWS Lambda functions
-resource = "aws_lambda_function" "counter_lambda" {
-    function_name = "counter"
-    s3_bucket = "counter_s3_bucket"
-    s3_key = "counter.zip"
-    role = "aws_iam_role.counter_lambda_role.arn"
-    handler = "counter.handler"
-    runtime = "python3.8"
+resource "aws_lambda_function" "test_lambda" {
+  function_name = "counter"
+  s3_bucket     = "adt-counter-s3-bucket"
+  s3_key        = "counter.zip"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "counter.handler"
+  runtime       = "python3.8"
 }
